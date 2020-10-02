3b90b3f670093fbc7c1e927915912373e82f363a
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-10-02 15:53:00 +0700

    basic functions

---
+++ toss-a-coin/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<div class="coin">{{ coinStatus }}</div>
+    <button @click="tossACoin">Toss</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	data() {
+		return { coinStatus: 'H' };
+  },
+  methods: {
+    tossACoin () {
+      const hOrT = 'HT';
+      this.coinStatus = hOrT[Math.floor(Math.random() * 2)];
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
